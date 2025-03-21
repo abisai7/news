4e60ee58f90ee33acbcfb07cb00db2a2b5048b35
Abisai Herrera <d_abisai7@outlook.com>
2025-03-21 16:03:50 -0600

    configure guard route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { NewsComponent } from './pages/news/news.component';
 import { NewsDetailComponent } from './pages/news-detail/news-detail.component';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
-    { path: 'news', component: NewsComponent },
-    { path: 'news/:id', component: NewsDetailComponent },
+    { path: 'news', component: NewsComponent, canActivate: [authGuard] },
+    { path: 'news/:id', component: NewsDetailComponent, canActivate: [authGuard] },
     { path: '**', redirectTo: '/login' }
 ];
